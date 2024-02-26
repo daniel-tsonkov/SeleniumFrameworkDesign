b00d4104fd4506d97cb681dbd5c19d6a50162092
dan <daniel_tzonkov@abv.bg>
2024-02-26 15:43:09 +0200

    26022024

--- src/main/java/rahulshettyacademy/StandAloneTest.java
+++ src/main/java/rahulshettyacademy/StandAloneTest.java
@@ -4,12 +4,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class StandAloneTest
 {
     public static void main( String[] args )
     {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/client/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        System.exit(0);
     }
 }
