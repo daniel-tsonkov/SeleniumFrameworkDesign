e139546e5da14535520047490d18da1865d8ff4c
dan <daniel_tzonkov@abv.bg>
2024-02-26 15:48:09 +0200

    26022024

--- src/main/java/rahulshettyacademy/StandAloneTest.java
+++ src/main/java/rahulshettyacademy/StandAloneTest.java
@@ -1,21 +1,23 @@
 package rahulshettyacademy;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class StandAloneTest
-{
-    public static void main( String[] args )
-    {
+public class StandAloneTest {
+    public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/client/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+        driver.findElement(By.id("userEmail")).sendKeys("testmilenatson@test.bg");
+        driver.findElement(By.id("userPassword")).sendKeys("12345678A1!a");
+        driver.findElement(By.id("login")).click();
 
         System.exit(0);
     }
