c6db59d2559f69e5d5491b0a86f8ddc08e5a6f8e
dan <daniel_tzonkov@abv.bg>
2024-03-08 09:44:46 +0200

    08032024

--- src/main/java/rahulshettyacademy/StandAloneTest.java
+++ src/main/java/rahulshettyacademy/StandAloneTest.java
@@ -21,7 +21,6 @@ public class StandAloneTest {
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/client/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         driver.findElement(By.id("userEmail")).sendKeys("testmilenatson@test.bg");
         driver.findElement(By.id("userPassword")).sendKeys("12345678A1!a");
         driver.findElement(By.id("login")).click();
