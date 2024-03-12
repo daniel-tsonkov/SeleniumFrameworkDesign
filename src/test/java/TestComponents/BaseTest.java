645d4b7a4c2e53533c220d2ddf25a70d6b42d1b3
dan <daniel_tzonkov@abv.bg>
2024-03-12 10:49:46 +0200

    12032024

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -4,10 +4,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseTest {
-    public void initializeDriver() {
+    public void initializeDriver() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream("src/main/java/rahulshettyacademy/recources/GlobalData.properties");
+        prop.load(fis);
+        String browserName = prop.getProperty("browser");
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
