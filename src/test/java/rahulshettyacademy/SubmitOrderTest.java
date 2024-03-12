7b424079abb8c7d1ca004761f80842487fc6bcc5
dan <daniel_tzonkov@abv.bg>
2024-03-12 14:45:39 +0200

    12032024

--- src/test/java/rahulshettyacademy/SubmitOrderTest.java
+++ src/test/java/rahulshettyacademy/SubmitOrderTest.java
@@ -1,26 +1,16 @@
 package rahulshettyacademy;
 
 import TestComponents.BaseTest;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import rahulshettyacademy.pageobjects.*;
 
-import java.io.IOException;
-import java.time.Duration;
-import java.util.List;
-
 public class SubmitOrderTest extends BaseTest{
-    //public static void main(String[] args) throws InterruptedException{
     @Test
-    public void submitOrder() throws InterruptedException, IOException {
+    public void submitOrder() throws InterruptedException {
         String productName = "ZARA COAT 3";
         ProductCatalogue productCatalogue = landingPage.loginApplication("testmilenatson@test.bg", "12345678A1!a");
 
-        List<WebElement> products = productCatalogue.getProductList();
         productCatalogue.addProductToCart(productName);
 
         CardPage cartPage = productCatalogue.goToCartPage();
@@ -32,5 +22,10 @@ public class SubmitOrderTest extends BaseTest{
 
         String confirMessage = confirmationPage.getConfirmatonMessage();
         Assert.assertTrue(confirMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));//second way
+    }
+
+    @Test(dependsOnMethods = {"submitOrder"})
+    public void orderHistoryTest() {
+
     }
 }
