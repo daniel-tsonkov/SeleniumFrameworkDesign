8cb8c2a1095b5bcf6df8cd45515b7ca5ed795ebf
dan <daniel_tzonkov@abv.bg>
2024-03-13 16:47:09 +0200

    13032024

--- src/test/java/rahulshettyacademy/data/DataReader.java
+++ src/test/java/rahulshettyacademy/data/DataReader.java
@@ -1,16 +1,22 @@
 package rahulshettyacademy.data;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class DataReader {
     public String getJsonDataMap() throws IOException {
         //read JSON file
         String jsonContent = FileUtils.readFileToString(new File(System.getProperty("user.dir")+"//src//test//java//rahulshettyacademy//data//PurchaseOrder.json"));
 
-        //convert to hashMap
-
+        //convert to hashMap this use "mvn Jackson Databind"
+        ObjectMapper mapper = new ObjectMapper();
+        List<HashMap<String, String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {
+        });
     }
 }
