9ffd306ef1f86f47050035e1d762dc8333cfec25
dan <daniel_tzonkov@abv.bg>
2024-03-15 11:00:09 +0200

    15032024

--- src/main/java/rahulshettyacademy/resources/ExtentReporterNG.java
+++ src/main/java/rahulshettyacademy/resources/ExtentReporterNG.java
@@ -1,7 +1,18 @@
 package rahulshettyacademy.resources;
 
-public class ExtentReporterNG {
-    public void getReportObject() {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+public class ExtentReporterNG {
+    ExtentReports extent;
+    public void getReportObject() {
+        String path = System.getProperty("user.dir") + "\\reports\\index.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setReportName("Web Automation Results");
+        reporter.config().setDocumentTitle("Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "My name should be here :)");
     }
 }
