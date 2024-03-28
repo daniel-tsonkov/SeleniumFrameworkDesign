3d4db5c96fc26845cd3fd4f21996beedf5a6fdfe
dan <daniel_tzonkov@abv.bg>
2024-03-28 12:51:17 +0200

    28032024

---
+++ src/test/java/rahulshettyacademy/TestComponents/Retry.java
@@ -0,0 +1,12 @@
+package rahulshettyacademy.TestComponents;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer {
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        return false;
+    }
+}
