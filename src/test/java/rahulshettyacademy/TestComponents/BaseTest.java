41fb0be00b14db8cc82eb3738327a929a26f73e2
dan <daniel_tzonkov@abv.bg>
2024-04-11 10:38:56 +0300

    11042024

--- src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
+++ src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import rahulshettyacademy.pageobjects.LandingPage;
@@ -31,12 +32,14 @@ public class BaseTest {
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//main//java//rahulshettyacademy//resources//GlobalData.properties");
         prop.load(fis);
         //String browserName = prop.getProperty("browser");
-        String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") : prop.getProperty("browser");
+        String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
 
         if (browserName.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
         } else if (browserName.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "/User/userName//documents//geckodriver");
+            driver = new FirefoxDriver();
             System.out.println("firefox");
         } else if (browserName.equalsIgnoreCase("edge")) {
             System.setProperty("webdriver.edge.driver", "edge.exe");
@@ -62,7 +65,7 @@ public class BaseTest {
     }
 
     public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
-        TakesScreenshot ts = (TakesScreenshot)driver;
+        TakesScreenshot ts = (TakesScreenshot) driver;
         File source = ts.getScreenshotAs(OutputType.FILE);
         File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
         FileUtils.copyFile(source, file);
