6a1313546ce01472457fcd6cf82630fa0ebacc3d
dan <daniel_tzonkov@abv.bg>
2024-04-11 10:26:51 +0300

    11042024

--- src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
+++ src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
@@ -30,7 +30,8 @@ public class BaseTest {
         Properties prop = new Properties();
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//main//java//rahulshettyacademy//resources//GlobalData.properties");
         prop.load(fis);
-        String browserName = prop.getProperty("browser");
+        //String browserName = prop.getProperty("browser");
+        String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") : prop.getProperty("browser");
 
         if (browserName.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
