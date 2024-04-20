aecc836e9856c28c42acfdd25521b0af58899e23
Daniel <daniel_tzonkov@abv.bg>
2024-04-20 13:16:36 +0300

    20042024

--- src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
+++ src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -34,9 +36,17 @@ public class BaseTest {
         //String browserName = prop.getProperty("browser");
         String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
 
-        if (browserName.equalsIgnoreCase("chrome")) {
+        if (browserName.contains("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            if(browserName.contains("headless")) {
+                options.addArguments("headless");
+            }
+
+            driver = new ChromeDriver(options);
+
+            driver.manage().window().setSize(new Dimension(1440, 900));
         } else if (browserName.equalsIgnoreCase("firefox")) {
             System.setProperty("webdriver.gecko.driver", "/User/userName//documents//geckodriver");
             driver = new FirefoxDriver();
